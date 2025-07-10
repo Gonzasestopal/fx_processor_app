a74d8a430af591292ae34f0a75b7dbeb999d4101
gonzasestopal <gonzasestopal@gmail.com>
2025-07-10 14:29:41 -0600

    chore[tests]: withdraw funds

---
+++ tests/test_withdraw_account.py
@@ -0,0 +1,15 @@
+
+def test_not_sufficient_balance():
+    assert True
+
+
+def test_not_existing_account():
+    assert True
+
+
+def test_not_existing_currency():
+    assert True
+
+
+def test_new_updated_balance():
+    assert True
