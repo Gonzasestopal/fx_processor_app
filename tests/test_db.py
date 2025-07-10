6707c7fe54013970ae880453ff217c633229abb1
gonzasestopal <gonzasestopal@gmail.com>
2025-07-09 20:43:50 -0600

    chore: missing edge cases for insert storage

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,3 +1,5 @@
+import pytest
+
 from src.db import Memory
 from src.models import Account
 
@@ -53,3 +55,90 @@ def test_insert():
     memory.insert('accounts', account.model_dump())
 
     assert memory.get_table('accounts') == [account.model_dump()]
+
+
+def test_insert_new_obj_id():
+    Memory._instance = None
+
+    account = Account(
+        id=1,
+        user_id=1,
+        currency_id=1,
+        amount=100,
+    )
+
+    memory = Memory(storage={'accounts': [account.model_dump()]})
+
+    new_obj = memory.insert('accounts', {'user_id': 1, 'currency_id': '2', 'amount': 0})
+
+    assert new_obj['id'] == 2
+
+
+def test_insert_clash_unique():
+    Memory._instance = None
+
+    account = Account(
+        id=1,
+        user_id=1,
+        currency_id=1,
+        amount=100,
+    )
+
+    memory = Memory(storage={'accounts': [account.model_dump()]})
+
+    with pytest.raises(Exception):
+        memory.insert('accounts', {'user_id': 1, 'currency_id': '1', 'amount': 0})
+
+
+def test_insert_new_obj_id_non_consecutive_records():
+    Memory._instance = None
+
+    account = Account(
+        id=1,
+        user_id=1,
+        currency_id=1,
+        amount=100,
+    )
+
+    another_account = Account(
+        id=3,
+        user_id=3,
+        currency_id=1,
+        amount=100,
+    )
+
+    memory = Memory(storage={'accounts': [account.model_dump(), another_account.model_dump()]})
+
+    new_obj = memory.insert('accounts', {'user_id': 1, 'currency_id': '1', 'amount': 0})
+
+    assert new_obj['id'] == 4
+
+
+def test_find_one():
+    Memory._instance = None
+
+    memory = Memory()
+
+    record = memory.find_one('currencies', id=1)
+
+    assert record == {'id': 1, 'name': 'MXN'}
+
+
+def test_find_one_by_key():
+    Memory._instance = None
+
+    memory = Memory()
+
+    record = memory.find_one('currencies', name='MXN')
+
+    assert record == {'id': 1, 'name': 'MXN'}
+
+
+def test_find_one_not_found():
+    Memory._instance = None
+
+    memory = Memory()
+
+    record = memory.find_one('currencies', name='JPN')
+
+    assert record == None
