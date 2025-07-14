ac33a0591de81b231cd9de2bf7facba3f5993daf
gonzasestopal <gonzasestopal@gmail.com>
2025-07-14 17:14:41 -0600

    feat: get transactions

--- src/handlers/get_transactions.py
+++ src/handlers/get_transactions.py
@@ -1,5 +1,14 @@
+from fastapi import HTTPException
+
 from src.db import memory_storage
 
 
 def get_transactions(user_id, storage=memory_storage):
-    return []
+    account = storage.find_one('accounts', user_id=user_id)
+
+    if not account:
+        raise HTTPException(status_code=404, detail='Account not found')
+
+    transactions = storage.find('transactions', account_id=account['id'])
+
+    return transactions
--- tests/test_get_transactions.py
+++ tests/test_get_transactions.py
@@ -4,6 +4,7 @@ import pytest
 from fastapi import HTTPException
 
 from src.handlers.get_transactions import get_transactions
+from src.models import Account, Transaction
 
 
 def test_account_not_found():
@@ -14,6 +15,48 @@ def test_account_not_found():
 
 def test_transactions_return_value():
     user_id = 1
-    transactions = get_transactions(user_id)
 
-    assert isinstance(transactions, list)
+    transaction = Transaction(
+        id=1,
+        type='debit',
+        account_id=1,
+        currency_id=1,
+        amount=100,
+    )
+
+    another_transaction = Transaction(
+        id=2,
+        type='credit',
+        account_id=1,
+        currency_id=1,
+        amount=50,
+    )
+
+    different_account_transaction = Transaction(
+        id=3,
+        type='debit',
+        account_id=2,
+        currency_id=1,
+        amount=100,
+    )
+
+    account = Account(
+        id=1,
+        user_id=1,
+        amount=100,
+        currency_id=1,
+    )
+
+    storage = Mock()
+    storage.find_one.return_value = account.model_dump()
+    storage.find.return_value = [
+        transaction.model_dump(),
+        another_transaction.model_dump(),
+        different_account_transaction.model_dump(),
+    ]
+
+    transactions = get_transactions(user_id, storage=storage)
+
+    assert transaction.model_dump() in transactions
+    assert another_transaction.model_dump() in transactions
+    assert different_account_transaction.model_dump() in transactions
