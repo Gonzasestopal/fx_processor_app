c550a32b0589f58e4b6904f37fb4a1156c5ca14d
gonzasestopal <gonzasestopal@gmail.com>
2025-07-14 20:59:31 -0600

    feat: async task for rate fluctuation

--- src/handlers/__init__.py
+++ src/handlers/__init__.py
@@ -1,5 +1,6 @@
 from src.handlers.convert_currency import convert_currency
 from src.handlers.fund_account import fund_account
 from src.handlers.get_transactions import get_transactions
+from src.handlers.update_rates import update_rates
 from src.handlers.view_balances import view_balances
 from src.handlers.withdraw_account import withdraw_account
--- src/main.py
+++ src/main.py
@@ -1,8 +1,45 @@
 
-from fastapi import FastAPI
+import random
+from contextlib import asynccontextmanager
 
+from fastapi import FastAPI
+from fastapi_utilities import repeat_at
+
+from src.handlers.update_rates import update_rates
 from src.routes import accounts
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    retrieve_new_fx()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(accounts.router, prefix='/wallets', tags=['Accounts'])
+
+
+@repeat_at(cron="* * * * *")
+def retrieve_new_fx():
+
+    new_rates = {
+        'MXN': {
+            'USD': random_float_0_05_0_06()
+        },
+        'USD': {
+            'MXN': random_float_18_20()
+        }
+    }
+
+    update_rates(new_rates=new_rates)
+    print(new_rates)
+
+
+def random_float_18_20():
+    return round(random.uniform(18, 20), 2)
+
+
+def random_float_0_05_0_06():
+    rand_int = random.randint(50, 60)
+    return rand_int / 1000
