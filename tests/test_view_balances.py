c6ddf9f2296daf9bca0c3f07b33707481a52fd62
gonzasestopal <gonzasestopal@gmail.com>
2025-07-14 19:14:16 -0600

    fix[tests]: add decimal conversion for balance

--- tests/test_view_balances.py
+++ tests/test_view_balances.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 from unittest.mock import Mock
 
 import pytest
@@ -24,3 +25,19 @@ def test_return_value():
     user_id = 1
     response = view_balances(user_id, storage)
     assert isinstance(response, dict)
+
+
+def test_decimal_conversions():
+    storage = Mock()
+    storage.find.return_value = [
+        {'id': 1, 'user_id': 1, 'currency_id': 1, 'amount': Decimal('13.01')},
+        {'id': 2, 'user_id': 1, 'currency_id': 2, 'amount': Decimal('9.99')},
+    ]
+    storage.find_one.side_effect = [{'id': 1, 'name': 'MXN'}, {'id': 2, 'name': 'USD'}]
+    user_id = 1
+    response = view_balances(user_id, storage)
+
+    assert response == {
+        'MXN': 13,
+        'USD': 10,
+    }
